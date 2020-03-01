b2a43f05eedd764b4b39ea9499d57500ed65a1b6
DanielComboni <daniel@laboremus.no>
2020-03-01 13:49:24 +0300

    project initialized

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,10 +22,14 @@ namespace DotNetChatApp
 
         public IConfiguration Configuration { get; }
 
+        string conn = "Server=tcp:hsportsdaniel.database.windows.net,1433;Initial Catalog=DotNetChat;Persist Security Info=False;User ID=daniel;Password=Certified.101;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<Context>(options => options.UseSqlServer(Configuration.GetConnectionString(conn)));
             services.AddControllers();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
