4116d6200a3ca2227e1b9cb90fa487e5baf2e85d
DanielComboni <daniel@laboremus.no>
2020-03-01 15:47:47 +0300

    first migration test

---
+++ Model/Context/ModelCreatings/Users/UserModelCreating.cs
@@ -0,0 +1,40 @@
+﻿using DotNetChatApp.Model.Entity.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetChatApp.Model.Context.ModelCreatings.Users
+{
+    public class UserModelCreating
+    {
+        public static void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            GetBuilder(modelBuilder).ToTable("users");
+            
+            GetBuilder(modelBuilder).HasKey(u => u.Id);
+            GetBuilder(modelBuilder).Property(u => u.Id)
+                .HasColumnName("Id")
+                .HasColumnType<long>("bigint")
+                .IsRequired();
+
+            GetBuilder(modelBuilder).Property(u => u.Email)
+                .HasColumnName("email")
+                .HasColumnType<string>("varchar(128)")
+                .IsRequired();
+
+            GetBuilder(modelBuilder).Property(u => u.Password)
+                .HasColumnName("password")
+                .HasColumnType<string>("varchar(255)")
+                .IsRequired();
+        }
+
+        private static EntityTypeBuilder<User> GetBuilder(ModelBuilder modelBuilder)
+        {
+            return modelBuilder.Entity<User>();
+        }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetChatApp.Model.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -22,16 +23,15 @@ namespace DotNetChatApp
 
         public IConfiguration Configuration { get; }
 
-        string conn = "Server=tcp:hsportsdaniel.database.windows.net,1433;Initial Catalog=DotNetChat;Persist Security Info=False;User ID=daniel;Password=Certified.101;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        //string conn = "Server=tcp:hsportsdaniel.database.windows.net,1433;Initial Catalog=DotNetChat;Persist Security Info=False;User ID=daniel;Password=Certified.101;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContextPool<Context>(options => options.UseSqlServer(Configuration.GetConnectionString(conn)));
+            services.AddDbContextPool<ChatDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DotNetChatAppDBConnection")));
             services.AddControllers();
-
         }
-
+        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
